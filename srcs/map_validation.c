69e30b85170747dd3ee3865c1666a1eac18450e8
Anas Ajaanan <114141528+anassajaanan@users.noreply.github.com>
2023-11-05 06:59:55 +0000

    Finish validating the map

--- srcs/map_validation.c
+++ srcs/map_validation.c
@@ -6,18 +6,73 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 04:54:38 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/11/05 06:23:01 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/11/05 06:59:07 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int	is_enclosed_map(t_map *map);
-
-static int		is_valid_map_char(char c)
+static int	is_valid_map_char(char c)
 {
 	return (c == '0' || c == '1' || c == ' ' || c == 'N' || c == 'S' || c == 'E'
 		|| c == 'W');
+}
+
+static int	has_valid_neighbors(t_map *map, int i, int j)
+{
+	if (map->map_data[i][j + 1] == ' '
+		|| map->map_data[i][j - 1] == ' '
+		|| map->map_data[i + 1][j] == ' '
+		|| map->map_data[i - 1][j] == ' '
+		|| map->map_data[i + 1][j + 1] == ' '
+		|| map->map_data[i + 1][j - 1] == ' '
+		|| map->map_data[i - 1][j + 1] == ' '
+		|| map->map_data[i - 1][j - 1] == ' ')
+		return (0);
+	return (1);
+}
+
+static int	validate_map_element(t_map *map, int i, int j)
+{
+	if (i == 0 || i == map->rows - 1 || j == 0 || j == map->cols - 1)
+	{
+		if (map->map_data[i][j] != '1' && map->map_data[i][j] != ' ')
+		{
+			ft_printf_fd(2, "Error\nMap is not enclosed 1\n");
+			return (0);
+		}
+	}
+	else if (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
+	{
+		if (!has_valid_neighbors(map, i, j))
+		{
+			ft_printf_fd(2, "Error\nMap is not enclosed 2: row: %d, \
+col: %d\n", i, j);
+			return (0);
+		}
+	}
+	return (1);
+}
+
+static int	is_enclosed_map(t_map *map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (map->map_data[i])
+	{
+		j = 0;
+		while (map->map_data[i][j])
+		{
+			if (!validate_map_element(map, i, j))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
 int	validate_map_characters(t_map *map)
@@ -35,10 +90,8 @@ int	validate_map_characters(t_map *map)
 		while (map->map_data[i][j])
 		{
 			if (!is_valid_map_char(map->map_data[i][j]))
-			{
-				ft_printf_fd(2, "Error\nInvalid character in map: %c\n", map->map_data[i][j]);
-				return (0);
-			}
+				return (ft_printf_fd(2, "Error\nInvalid character in map: %c\n",
+						map->map_data[i][j]), 0);
 			if (map->map_data[i][j] == 'N' || map->map_data[i][j] == 'S'
 				|| map->map_data[i][j] == 'E' || map->map_data[i][j] == 'W')
 				start_count++;
@@ -47,44 +100,7 @@ int	validate_map_characters(t_map *map)
 		i++;
 	}
 	if (start_count != 1)
-	{
-		ft_printf_fd(2, "Error\nInvalid number of starting positions\n");
-		return (0);
-	}
+		return (ft_printf_fd(2, "Error\nInvalid number of starting \
+positions\n"), 0);
 	return (is_enclosed_map(map));
-}
-
-int	is_enclosed_map(t_map *map)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (map->map_data[i])
-	{
-		j = 0;
-		while (map->map_data[i][j])
-		{
-			if (i == 0 || i == map->rows - 1 || j == 0 || j == map->cols - 1)
-			{
-				if (map->map_data[i][j] != '1' && map->map_data[i][j] != ' ')
-				{
-					ft_printf_fd(2, "Error\nMap is not enclosed 1\n");
-					return (0);
-				}
-			}
-			else if (map->map_data[i][j] == '0' || map->map_data[i][j] == 'N' || map->map_data[i][j] == 'S' || map->map_data[i][j] == 'E' || map->map_data[i][j] == 'W')
-			{
-				if (map->map_data[i][j + 1] == ' ' || map->map_data[i][j - 1] == ' ' || map->map_data[i + 1][j] == ' ' || map->map_data[i - 1][j] == ' ' || map->map_data[i + 1][j + 1] == ' ' || map->map_data[i + 1][j - 1] == ' ' || map->map_data[i - 1][j + 1] == ' ' || map->map_data[i - 1][j - 1] == ' ')
-				{
-					ft_printf_fd(2, "Error\nMap is not enclosed 2: row: %d, col: %d\n", i, j);
-					return (0);
-				}
-			}
-			j++;
-		}
-		i++;
-	}
-	return (1);
 }
